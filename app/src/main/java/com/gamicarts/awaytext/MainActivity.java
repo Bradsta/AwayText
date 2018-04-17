7a26eccb5b7f009461fe946d509e8b8c1afb5229
Jewst <jmiranda5428@gmail.com>
2018-04-16 17:13:35 -0700

    SMS reciever/sender added

--- app/src/main/java/com/gamicarts/awaytext/MainActivity.java
+++ app/src/main/java/com/gamicarts/awaytext/MainActivity.java
@@ -2,12 +2,34 @@ package com.gamicarts.awaytext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean awayTextOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button awayTextButton = findViewById(R.id.awayTextButton);
+        awayTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (awayTextOn)
+                {
+                    awayTextButton.setText("AWAY TEXT: OFF");
+                }
+                else
+                {
+                    awayTextButton.setText("AWAY TEXT: ON");
+                }
+
+                awayTextOn = !awayTextOn;
+            }
+        });
     }
+
 }
